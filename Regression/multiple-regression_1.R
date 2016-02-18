b4b0c2a05cde1ac6f844a2d735085c3c5180c249
Ariuntulga Tserendavaa <tserenda@hotmail.com>
2016-02-17 21:20:46 -0800

    DONE multiple linear regression 1 in R and iPython

--- Regression/multiple-regression_1.R
+++ Regression/multiple-regression_1.R
@@ -1,43 +1,43 @@
-test = read.csv("data\\kc_house_test_data.csv")
-train = read.csv("data\\kc_house_train_data.csv")
+train_data = read.csv('data/week2/kc_house_train_data.csv')
+test_data = read.csv('data/week2/kc_house_test_data.csv')
 
-# transformations and interactions of existing variables
-train$bedrooms_squared = train$bedrooms ^ 2
-train$bed_bath_rooms = train$bedrooms * train$bathrooms
-train$log_sqft_living = log(train$sqft_living)
-train$lat_plus_long = train$lat + train$long
+# transformations & interactions of existing variables
+train_data$bedrooms_squared = train_data$bedrooms^2
+train_data$bed_bath_rooms = train_data$bedrooms * train_data$bathrooms
+train_data$log_sqft_living = log(train_data$sqft_living)
+train_data$lat_plus_long = train_data$lat + train_data$long
 
-test$bedrooms_squared = test$bedrooms ^ 2
-test$bed_bath_rooms = test$bedrooms * test$bathrooms
-test$log_sqft_living = log(test$sqft_living)
-test$lat_plus_long = test$lat + test$long
+test_data$bedrooms_squared = test_data$bedrooms ^ 2
+test_data$bed_bath_rooms = test_data$bedrooms * test_data$bathrooms
+test_data$log_sqft_living = log(test_data$sqft_living)
+test_data$lat_plus_long = test_data$lat + test_data$long
 
 # calculate the means
-cat("Q1: ", round(sapply(test[, c("bedrooms_squared", "bed_bath_rooms", "log_sqft_living", "lat_plus_long")], mean), 2))
+means = colMeans(test_data[, c('bedrooms_squared', 'bed_bath_rooms', 'log_sqft_living', 'lat_plus_long')])
+cat('Q1:', round(means, 2))
 
 # fit different models
-model1 = lm(price ~ sqft_living+bedrooms+bathrooms+lat+long, data = train)
-model2 = lm(price ~ sqft_living+bedrooms+bathrooms+lat+long+bed_bath_rooms, data = train)
-model3 = lm(price ~ sqft_living+bedrooms+bathrooms+lat+long+bed_bath_rooms+bedrooms_squared+log_sqft_living+lat_plus_long, data = train)
+mod1 = lm(price ~ sqft_living + bedrooms + bathrooms + lat + long, train_data)
+mod2 = lm(price ~ sqft_living + bedrooms + bathrooms + lat + long + bed_bath_rooms, train_data)
+mod3 = lm(price ~ sqft_living + bedrooms + bathrooms + lat + long + bed_bath_rooms + bedrooms_squared + log_sqft_living + lat_plus_long, train_data)
 
-cat("Q2: 1st coeff for bathrooms", model1$coefficients["bathrooms"])
-cat("Q3: 2nd coeff for bathrooms", model2$coefficients["bathrooms"])
+cat('\nQ2: bathroom weight of the 1st model=', round(mod1$coefficients['bathrooms']))
+cat('\nQ3: bathroom weight of the 2nd model=', round(mod2$coefficients['bathrooms']))
+
+RSS <- function(data, fit) {
+    predictions = predict(fit, data)
+    rss <- sum((data$price - predictions)^2)
+    return(rss)
+}
 
 # calculate the RSS on the training data
-train_RSS1 = sum((train$price - predict(model1, train)) ^ 2)
-train_RSS2 = sum((train$price - predict(model2, train)) ^ 2)
-train_RSS3 = sum((train$price - predict(model3, train)) ^ 2)
-cat("Q4: ")
-cat("train1=", train_RSS1)
-cat("train2=", train_RSS2)
-cat("train3=", train_RSS3)
+train_RSS1 = RSS(train_data, mod1)
+train_RSS2 = RSS(train_data, mod2)
+train_RSS3 = RSS(train_data, mod3)
+cat('\nQ4:', train_RSS1, train_RSS2, train_RSS3)
 
 # calculate the RSS on the test data
-test_RSS1 = sum((test$price - predict(model1, test)) ^ 2)
-test_RSS2 = sum((test$price - predict(model2, test)) ^ 2)
-test_RSS3 = sum((test$price - predict(model3, test)) ^ 2)
-cat("Q5: ")
-cat("test1=", test_RSS1)
-cat("test2=", test_RSS2)
-cat("test3=", test_RSS3)
-
+test_RSS1 = RSS(test_data, mod1)
+test_RSS2 = RSS(test_data, mod2)
+test_RSS3 = RSS(test_data, mod3)
+cat('\nQ5:', test_RSS1, test_RSS2, test_RSS3)
